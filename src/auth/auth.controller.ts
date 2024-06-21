ad419afb67803d745370340ca5079bc5b37e4c24
Binod Gautam <gautambinod629@gmail.com>
2024-06-21 15:45:25 +0545

    add login functionality

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, UseGuards ,Request} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './jwt-auth.guard';
+
+@Controller()
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user)
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
